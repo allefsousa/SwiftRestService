467c45fd8753ade9e04b4b96763de72e74a4b3e0
Allef Sousa <allefsousa_1@hotmail.com>
2019-02-17 14:16:43 -0300

    Atualizando a view com os dados da requisição

--- Precobit/ViewController.swift
+++ Precobit/ViewController.swift
@@ -10,9 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func updatePreco(_ sender: Any) {
+        self.recuperarPrecoBitcoin()
+    }
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var preco: UIButton!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        self.recuperarPrecoBitcoin()
+    }
+    
+    func formatterPreco(preco:NSNumber) -> String {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        if let precoFinal = nf.string(from: preco){
+            return precoFinal
+        }
+        return "0,00"
+    }
+    
+    func recuperarPrecoBitcoin(){
+        
+        
+        self.preco.setTitle("Atualizando .......", for: .normal)
+        
+        if let url = URL(string: "https://blockchain.info/ticker"){
+            // tarefa para consultar o serviço executada em uma thread diferente
+            let tarefa = URLSession.shared.dataTask(with: url){(dados,requisicao,erro) in
+                
+                if erro == nil{
+                    print("Sucesso ao fazer consulta de preço")
+                    if let dadosRetorno = dados{
+                        
+                        do{
+                            if let objectJson = try JSONSerialization.jsonObject(with: dadosRetorno, options:[]) as? [String: Any]{
+                                
+                                if let blr = objectJson["BRL"] as? [String:Any]{
+                                    if let pree = blr["buy"] as? Double{
+                                        let precoFomatado = self.formatterPreco(preco: NSNumber(value:pree))
+                                       
+                                        /// atualizando a main Theread
+                                        DispatchQueue.main.sync(execute: {
+                                              self.labelPreco.text = "R$" + precoFomatado
+                                            self.preco.setTitle("Atualizar", for: .normal)
+
+                                                })
+                                        
+                                      
+                                        
+                                    }
+                                    
+                                    
+                                }
+                                
+                            }
+                            
+                        }catch{
+                            print("Erro ao formatar o retorno")
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }else{
+                    print("Erro ao fazer consulta de preço")
+                    
+                }
+                
+            }
+            tarefa.resume()
+            
+        }
     }
 
 
